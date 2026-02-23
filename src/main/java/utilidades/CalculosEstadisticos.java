f8dbf25a6a8ba7b3cb9b38e97c05c419d9fbc5f0
JesusAntonioja27 <l24170589@culiacan.tecnm.mx>
2026-02-22 20:53:14 -0700

    Implementacion de Prueba Ji Cuadrado

--- src/main/java/utilidades/CalculosEstadisticos.java
+++ src/main/java/utilidades/CalculosEstadisticos.java
@@ -1,12 +1,31 @@
 package utilidades;
 
-/**
- * Clase auxiliar que contiene el método 'calcularJiCuadrada'
- * para ser reciclado por las clases del paquete 'pruebas'.
- */
 public class CalculosEstadisticos {
-    public static double calcularJiCuadrada() {
-        // Lógica compartida de cálculos
-        return 0.0;
+    public static int[] calcularJiCuadrada(double[] numerosAleatorios, int m) {
+        int[] frecuenciasObservadas = new int[m];
+        double tamañoIntervalo = 1.0 / m;
+        for (int i = 0; i < numerosAleatorios.length; i++) {
+            int indice = (int) (numerosAleatorios[i] / tamañoIntervalo);
+
+            if (indice >= m) {
+                indice = m - 1;
+            }
+
+            frecuenciasObservadas[indice]++;
+        }
+        return frecuenciasObservadas;
+    }
+
+    // metodo para aplicar la formula: sumatoria de ((FO-FE)al cuadrado / FE)
+    public static double calcularEstadisticoJiCuadrada(int[] frecuenciasObservadas, double frecuenciaEsperada) {
+        double estadisticoC = 0;
+        for (int i = 0; i < frecuenciasObservadas.length; i++) {
+            double diferencia = frecuenciasObservadas[i] - frecuenciaEsperada;
+            double diferenciaAlCuadrado = Math.pow(diferencia, 2);
+
+            estadisticoC = estadisticoC + (diferenciaAlCuadrado / frecuenciaEsperada);
+
+        }
+        return estadisticoC;
     }
 }
